18205cb87fe2ffedddadb0bf8c1b0a3836071dc9
Julian Casaburi <juliancasaburi@gmail.com>
2024-02-03 09:10:51 -0300

    💄 style: adjust layout to stack components on small screens

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ export default async function Home() {
   return (
     <>
       <section className="justify-center">
-        <div className="flex items-start w-full">
-          <div className="mr-10 mb-4 p-4 rounded-lg border w-full">
+        <div className="flex flex-col md:flex-row items-start w-full">
+          <div className="mb-4 p-4 rounded-lg border w-full lg:w-1/2 md:mr-4">
             <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-4">
               Calculadora de dólar oficial
             </h2>
             <Calculator />
           </div>
-          <div className="p-4 rounded-lg border w-full">
+          <div className="mb-4 p-4 rounded-lg border w-full lg:w-1/2">
             <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-4">
               Cotizacion histórica dólar oficial
             </h2>
